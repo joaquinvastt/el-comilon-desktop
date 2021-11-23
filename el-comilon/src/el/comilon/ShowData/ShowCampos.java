49a0f6cd69554eda4b1d3d80d69722dc6728a230
pxtxrsss <90583960+pxtxrsss@users.noreply.github.com>
2021-11-23 19:45:02 -0300

    2.0

--- el-comilon/src/el/comilon/ShowData/ShowCampos.java
+++ el-comilon/src/el/comilon/ShowData/ShowCampos.java
@@ -9,16 +9,23 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.CallableStatement;
+import java.sql.Timestamp;
+
+import java.awt.Toolkit;
+import java.util.concurrent.*;
 
 import el.comilon.SQLclass;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  *
  * @author Joaquin
  */
-public class ShowCampos {
-
-    public static void main(String[] args) {
+public class ShowCampos implements Runnable {
+    
+    public void run() {
+        
         String sSQL = "";
         SQLclass conexion = new SQLclass();
 
@@ -27,7 +34,11 @@ public class ShowCampos {
             Connection con = conexion.conectar();
             Statement cn = con.createStatement();
             ResultSet res = cn.executeQuery(sSQL);
-
+            
+            CallableStatement cstIN = con.prepareCall("{call SP_INSERT_REPORTE()}");
+            
+            cstIN.execute();
+            
             CallableStatement cst = con.prepareCall("{call OBTENER_REPORTE (?,?,?,?,?,?,?,?,?,?)}");
 
             cst.registerOutParameter(1, java.sql.Types.INTEGER);
@@ -42,22 +53,46 @@ public class ShowCampos {
             cst.registerOutParameter(10, java.sql.Types.TIMESTAMP);
 
             cst.execute();
-
-            while (res.next()) {
-                System.out.println("---------------------");
-                System.out.println("ID: " + cst.getInt(1));
-                System.out.println(cst.getInt(2));
-                System.out.println(cst.getInt(3));
-                System.out.println(cst.getInt(4));
-                System.out.println(cst.getInt(5));
-                System.out.println(cst.getInt(6));
-                System.out.println(cst.getString(7));
-                System.out.println(cst.getInt(8));
-                System.out.println(cst.getInt(9));
-                System.out.println(cst.getTimestamp(10));
-                System.out.println("---------------------");
-            }
+            
+            int ID = cst.getInt(1);
+            int montoProm = cst.getInt(2);
+            int cantVentas = cst.getInt(3);
+            int cantReclamo = cst.getInt(4);
+            int cantReclamoPend = cst.getInt(5);
+            int cantReclamoResu = cst.getInt(6);
+            String calidadReclamo = cst.getString(7);
+            int cantPedFinalizado = cst.getInt(8);
+            int cantPedCancelado = cst.getInt(9);
+            Timestamp fecha =  cst.getTimestamp(10);
+            
+            LocalDateTime fechaReporte = fecha.toLocalDateTime();
+            
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern(
+                "dd-MM-YYYY HH:mm:ss a");
+            
+            String dateTimeString = fechaReporte.format(formatter);
+            
+            
+            System.out.print(dateTimeString+"\n");
+            System.out.print("-----\n");
+            
         } catch (SQLException ex) {
             System.out.println("Error: " + ex.getMessage());
         }
-    }}
+        
+        Toolkit.getDefaultToolkit().beep();
+    }
+
+    public static void main(String[] args) {
+        
+        ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+
+        Runnable task = new ShowCampos();
+        int initialDelay = 0;
+        int periodicDelay = 1;
+        scheduler.scheduleAtFixedRate(task, initialDelay, periodicDelay,
+                TimeUnit.MINUTES
+        );
+        
+    }
+}
